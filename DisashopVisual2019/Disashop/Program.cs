b93d61a1cc02e516ee6b7ef7da8cf24c0b2d3b75
developDaniel <111102294+developDaniel@users.noreply.github.com>
2022-09-30 19:13:40 -0500

    disashop

---
+++ DisashopVisual2019/Disashop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ServiceReference1;
+
+namespace Disashop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ConsumirWebSevices();
+        }
+
+        private static void ConsumirWebSevices()
+        {
+            WSPITClient serv = new WSPITClient();
+            testRequest tec = new testRequest();
+            //testResponse rec;            
+
+            tec.Body.entidad = "TACN";
+            tec.Body.idCliente = "99154067";
+            tec.Body.idTerminal = "59154067";
+            tec.Body.referenciaCliente = "11130";
+
+
+            serv.test(tec);
+
+
+
+
+        }
+    }
+}
